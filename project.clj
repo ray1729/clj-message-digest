0046f382d3bb095bc5545f8467e5f92352a3ef3b
Ray Miller <ray@1729.org.uk>
2011-12-13 21:17:30 +0000

    Update version to 1.0.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-message-digest "1.0.0-SNAPSHOT"
+(defproject clj-message-digest "1.0.0"
   :description "Message digest (MD5, SHA-1, etc.) functions for Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [commons-codec "1.5"]])
